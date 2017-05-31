fbc8f472290565497f86b03da2101e0a8200ecb9
James De La Torre <jamesdelatorre@mail.com>
2017-05-31 11:27:59 -0400

    We have added a new python script. For now, it just contains initial commit.

---
+++ python_practice/practice_script.py
@@ -0,0 +1,5 @@
+'''
+This script contains an example class that we will write. The class will be called algebra, and it will contain some simple methods to do som algebraic manipulation.
+
+ The script will end with a 
+'''
